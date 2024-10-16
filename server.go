0030789d6c807def05a54122d075d2675cade5ff
Huiliyi <xiaoguaishouhuiliyi121@gmail.com>
2024-10-16 18:34:26 +0800

    实现简易的多人聊天服务器
    - 添加用户结构体(User)和在线用户列表
    - 实现用户广播消息的功能
    - 启动监听消息队列的goroutine
    - 优化处理客户端连接的逻辑，支持新用户加入和广播

--- server.go
+++ server.go
@@ -3,23 +3,57 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	//在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+	// 消息队列
+	Message chan string
 }
 
 // NewServer 创建一个服务器对象
 func NewServer(ip string, port int) *Server {
-	server := &Server{Ip: ip, Port: port}
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
 	return server
+}
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := fmt.Sprintf("[%s] %s", user.Name, msg)
+	s.Message <- sendMsg
+}
+
+func (s *Server) ListenMessage() {
+	for {
+		msg := <-s.Message
+		s.mapLock.Lock()
+		for _, cli := range s.OnlineMap {
+			cli.C <- msg
+		}
+		s.mapLock.Unlock()
+	}
 }
 
 // Handler 处理客户端的连接请求
 func (s *Server) Handler(conn net.Conn) {
 	// 处理客户端业务
 	fmt.Println("connected success")
+	user := NewUser(conn)
+	s.mapLock.Lock()
+	s.OnlineMap[user.Name] = user
+	s.mapLock.Unlock()
+
+	//广播新用户消息
+	s.BroadCast(user, fmt.Sprintf("%s come in", user.Name))
 }
 func (s *Server) Start() {
 	//socket listen
@@ -35,6 +69,9 @@ func (s *Server) Start() {
 			fmt.Println("close listener failed, err:", err)
 		}
 	}(listener)
+
+	//启动监听message
+	go s.ListenMessage()
 
 	for {
 		//accept
---
+++ user.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+// NewUser 创建一个用户的方法
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+	}
+	// 启动监听当前user channel消息的goroutine
+	go user.ListenMessage()
+	return user
+
+}
+
+// ListenMessage 监听当前用户 channel 的消息
+func (u *User) ListenMessage() {
+	for {
+		msg := <-u.C
+		write, err := u.conn.Write([]byte(msg + "\n"))
+		if err != nil {
+			fmt.Println("发送超时，时间为", write, err)
+		}
+	}
+}
