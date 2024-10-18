8a5f31ff2cb9df38ccbd48f2aae7b5081dc6ce00
Huiliyi <xiaoguaishouhuiliyi121@gmail.com>
2024-10-18 22:05:48 +0800

    feat(client): 添加命令行参数设置服务器 IP 和端口
    - 新增 -i 和 -p命令行参数，用于设置服务器 IP 地址和端口- 默认 IP 地址为127.0.0.1，端口为 8888
    - 优化了客户端初始化过程，支持动态配置服务器地址

--- client/main.go
+++ client/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
 
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "i", "127.0.0.1", "设置服务器IP地址(默认127.0.0.1)")
+	flag.IntVar(&serverPort, "p", 8888, "设置服务器端口(默认8888)")
+}
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	// 命令行解析
+	flag.Parse()
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>> 连接服务器失败")
 		return
